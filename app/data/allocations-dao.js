a416490658a959eb5c673439f09262e88b878f40
joeco <joe@joeco.info>
2017-02-18 16:18:01 -0500

    Updates es6 syntax of allocations-dao.js

--- app/data/allocations-dao.js
+++ app/data/allocations-dao.js
@@ -1,118 +1,118 @@
-var UserDAO = require("./user-dao").UserDAO;
+import { UserDAO } from "./user-dao";
 
 /* The AllocationsDAO must be constructed with a connected database object */
 function AllocationsDAO(db) {
 
-    "use strict";
+  "use strict";
 
-    /* If this constructor is called without the "new" operator, "this" points
-     * to the global object. Log a warning and call it correctly. */
-    if (false === (this instanceof AllocationsDAO)) {
-        console.log("Warning: AllocationsDAO constructor called without 'new' operator");
-        return new AllocationsDAO(db);
-    }
+  /* If this constructor is called without the "new" operator, "this" points
+   * to the global object. Log a warning and call it correctly. */
+  if (false === (this instanceof AllocationsDAO)) {
+    console.log("Warning: AllocationsDAO constructor called without 'new' operator");
+    return new AllocationsDAO(db);
+  }
 
-    var allocationsCol = db.collection("allocations");
-    var userDAO = new UserDAO(db);
+  const allocationsCol = db.collection("allocations");
+  const userDAO = new UserDAO(db);
 
 
-    this.update = function(userId, stocks, funds, bonds, callback) {
-        var finalId = parseInt(userId);
+  this.update = (userId, stocks, funds, bonds, callback) => {
+    const finalId = parseInt(userId);
 
-        // Create allocations document
-        var allocations = {
-            userId: userId,
-            stocks: stocks,
-            funds: funds,
-            bonds: bonds
-        };
-
-        allocationsCol.update({
-            userId: finalId
-        }, allocations, {
-            upsert: true
-        }, function(err, result) {
-
-            if (!err) {
-
-                console.log("Updated allocations");
-
-                userDAO.getUserById(userId, function(err, user) {
-
-                    if (err) return callback(err, null);
-
-                    // add user details
-                    allocations.userId = userId;
-                    allocations.userName = user.userName;
-                    allocations.firstName = user.firstName;
-                    allocations.lastName = user.lastName;
-
-                    return callback(null, allocations);
-                });
-            }
-
-            return callback(err, null);
-        });
+    // Create allocations document
+    const allocations = {
+      userId,
+      stocks,
+      funds,
+      bonds
     };
 
-    // This is the good implementation, respect the last one
-    this.getByUserId = function(userId, callback) {
-        var finalId = parseInt(userId);
+    allocationsCol.update({
+      userId: finalId
+    }, allocations, {
+      upsert: true
+    }, (err, result) => {
 
-        allocationsCol.findOne({
-            userId: finalId
-        }, function(err, allocations) {
-            if (err) return callback(err, null);
-            if (!allocations) return callback("ERROR: No allocations found for the user", null);
+      if (!err) {
 
-            userDAO.getUserById(finalId, function(err, user) {
-                if (err) return callback(err, null);
+        console.log("Updated allocations");
 
-                // add user details
-                allocations.userId = userId;
-                allocations.userName = user.userName;
-                allocations.firstName = user.firstName;
-                allocations.lastName = user.lastName;
+        userDAO.getUserById(userId, (err, user) => {
 
-                callback(null, allocations);
-            });
+          if (err) return callback(err, null);
 
+          // add user details
+          allocations.userId = userId;
+          allocations.userName = user.userName;
+          allocations.firstName = user.firstName;
+          allocations.lastName = user.lastName;
+
+          return callback(null, allocations);
         });
-    };
+      }
 
-    this.getThresholdByUserId = function(userId, threshold, callback) {
-        var finalId = parseInt(userId);
+      return callback(err, null);
+    });
+  };
 
-        /*
-        // Fix for A1 - 2 NoSQL Injection - escape the threshold parameter properly
-        Fix this SQL Injection which doesn't sanitze the input parameter 'threshold' and allows attackers
-        to inject arbitrary javascript code into the NoSQL query:
-        1. 0';while(true){}'
-        2. 1'; return 1 == 1
-        */
-        var whereClause = "this.userId == '" + finalId + "' && " + "this.stocks > " + "'" + threshold + "'";
+  // This is the good implementation, respect the last one
+  this.getByUserId = (userId, callback) => {
+    const finalId = parseInt(userId);
 
-        allocationsCol.findOne({
-            $where: whereClause
-        }, function(err, allocations) {
+    allocationsCol.findOne({
+      userId: finalId
+    }, (err, allocations) => {
+      if (err) return callback(err, null);
+      if (!allocations) return callback("ERROR: No allocations found for the user", null);
 
-            if (err) return callback(err, null);
-            if (!allocations) return callback("ERROR: No allocations found for the user", null);
+      userDAO.getUserById(finalId, (err, user) => {
+        if (err) return callback(err, null);
 
-            userDAO.getUserById(finalId, function(err, user) {
-                if (err) return callback(err, null);
+        // add user details
+        allocations.userId = userId;
+        allocations.userName = user.userName;
+        allocations.firstName = user.firstName;
+        allocations.lastName = user.lastName;
 
-                // add user details
-                allocations.userId = userId;
-                allocations.userName = user.userName;
-                allocations.firstName = user.firstName;
-                allocations.lastName = user.lastName;
+        callback(null, allocations);
+      });
 
-                callback(null, allocations);
-            });
+    });
+  };
 
-        });
-    };
+  this.getThresholdByUserId = (userId, threshold, callback) => {
+    const finalId = parseInt(userId);
+
+    /*
+     // Fix for A1 - 2 NoSQL Injection - escape the threshold parameter properly
+     Fix this SQL Injection which doesn't sanitze the input parameter 'threshold' and allows attackers
+     to inject arbitrary javascript code into the NoSQL query:
+     1. 0';while(true){}'
+     2. 1'; return 1 == 1
+     */
+    const whereClause = `this.userId == '${finalId}' && this.stocks > '${threshold}'`;
+
+    allocationsCol.findOne({
+      $where: whereClause
+    }, (err, allocations) => {
+
+      if (err) return callback(err, null);
+      if (!allocations) return callback("ERROR: No allocations found for the user", null);
+
+      userDAO.getUserById(finalId, (err, user) => {
+        if (err) return callback(err, null);
+
+        // add user details
+        allocations.userId = userId;
+        allocations.userName = user.userName;
+        allocations.firstName = user.firstName;
+        allocations.lastName = user.lastName;
+
+        callback(null, allocations);
+      });
+
+    });
+  };
 }
 
 module.exports.AllocationsDAO = AllocationsDAO;
