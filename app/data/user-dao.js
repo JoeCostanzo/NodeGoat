59c1eec8b294dc96d5ae42ac4e7d1b056608e5bf
joeco <joe@joeco.info>
2017-02-19 15:11:06 -0500

    discard redundant names.

--- app/data/user-dao.js
+++ app/data/user-dao.js
@@ -32,9 +32,9 @@ function UserDAO(db) {
 
         // Create user document
         const user = {
-          userName: userName,
-          firstName: firstName,
-          lastName: lastName,
+          userName,
+          firstName,
+          lastName,
           benefitStartDate: this.getRandomFutureDate(),
           password: hash
         };
