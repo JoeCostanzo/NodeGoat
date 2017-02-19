b8f53dd7cae4ec3e1f366e0d82b21e879e3184c3
joeco <joe@joeco.info>
2017-02-19 08:58:40 -0500

    indentation

--- app/routes/allocations.js
+++ app/routes/allocations.js
@@ -1,37 +1,37 @@
-import { AllocationsDAO } from "../data/allocations-dao";
+import {AllocationsDAO} from "../data/allocations-dao";
 
 function AllocationsHandler(db) {
-    "use strict";
+  "use strict";
 
-    const allocationsDAO = new AllocationsDAO(db);
+  const allocationsDAO = new AllocationsDAO(db);
 
-    this.displayAllocations = (req, res, next) => {
-        // Fix for A4 Insecure DOR -  take user id from session instead of from URL param
-        const userId = req.session.userId;
+  this.displayAllocations = (req, res, next) => {
+    // Fix for A4 Insecure DOR -  take user id from session instead of from URL param
+    const userId = req.session.userId;
 
-        allocationsDAO.getByUserId(userId, (err, docs) => {
-            if (err) return next(err);
+    allocationsDAO.getByUserId(userId, (err, docs) => {
+      if (err) return next(err);
 
-            docs.userId = userId; //set for nav menu items
+      docs.userId = userId; //set for nav menu items
 
-            return res.render("allocations", docs);
-        });
-    };
+      return res.render("allocations", docs);
+    });
+  };
 
-    this.displayAllocationsThreshold = (req, res, next) => {
+  this.displayAllocationsThreshold = (req, res, next) => {
 
-        const userId = req.session.userId;
-        const threshold = req.query.threshold || 0;
+    const userId = req.session.userId;
+    const threshold = req.query.threshold || 0;
 
-        allocationsDAO.getThresholdByUserId(userId, threshold, (err, docs) => {
+    allocationsDAO.getThresholdByUserId(userId, threshold, (err, docs) => {
 
-            if (err) return next(err);
+      if (err) return next(err);
 
-            docs.userId = userId; //set for nav menu items
+      docs.userId = userId; //set for nav menu items
 
-            return res.render("allocations", docs);
-        });
-    };
+      return res.render("allocations", docs);
+    });
+  };
 }
 
 module.exports = AllocationsHandler;
