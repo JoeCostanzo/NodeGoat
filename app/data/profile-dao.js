60ae7c392bd8b7e41c461047cfdac2acd32cfaf9
joeco <joe@joeco.info>
2017-02-20 08:27:55 -0500

    profile-dao.js ES6-ified, and crypto enabled as A6 fix.

--- app/data/profile-dao.js
+++ app/data/profile-dao.js
@@ -1,106 +1,98 @@
 /* The ProfileDAO must be constructed with a connected database object */
 function ProfileDAO(db) {
 
-    "use strict";
+  "use strict";
 
-    /* If this constructor is called without the "new" operator, "this" points
-     * to the global object. Log a warning and call it correctly. */
-    if (false === (this instanceof ProfileDAO)) {
-        console.log("Warning: ProfileDAO constructor called without 'new' operator");
-        return new ProfileDAO(db);
-    }
+  /* If this constructor is called without the "new" operator, "this" points
+   * to the global object. Log a warning and call it correctly. */
+  if (false === (this instanceof ProfileDAO)) {
+    console.log("Warning: ProfileDAO constructor called without 'new' operator");
+    return new ProfileDAO(db);
+  }
 
-    var users = db.collection("users");
+  const users = db.collection("users");
 
-    /* Fix for A6 - Sensitive Data Exposure
+  // Fix for A6 - Sensitive Data Exposure
 
-    // Use crypto module to save sensitive data such as ssn, dob in encrypted format
-    var crypto = require("crypto");
-    var config = require("../../config/config");
+  // Use crypto module to save sensitive data such as ssn, dob in encrypted format
+  import crypto from "crypto";
+  import config from "./../../dist/config/config";
 
-    // Helper function to encrypt data
-    var encrypt = function(toEncrypt) {
-        var cipher = crypto.createCipher(config.cryptoAlgo, config.cryptoKey);
-        return cipher.update(toEncrypt, "utf8", "hex") + cipher.final("hex");
-    };
+  // Helper function to encrypt data
+  const encrypt = toEncrypt => {
+    const cipher = crypto.createCipher(config.cryptoAlgo, config.cryptoKey);
+    return cipher.update(toEncrypt, "utf8", "hex") + cipher.final("hex");
+  };
 
-    // Helper function to decrypt data
-    var decrypt = function(toDecrypt) {
-        var decipher = crypto.createDecipher(config.cryptoAlgo, config.cryptoKey);
-        return decipher.update(toDecrypt, "hex", "utf8") + decipher.final("utf8");
-    };
+  // Helper function to decrypt data
+  const decrypt = toDecrypt => {
+    const decipher = crypto.createDecipher(config.cryptoAlgo, config.cryptoKey);
+    return decipher.update(toDecrypt, "hex", "utf8") + decipher.final("utf8");
+  };
 
-    */
 
-    this.updateUser = function(userId, firstName, lastName, ssn, dob, address, bankAcc, bankRouting, callback) {
+  this.updateUser = (
+    userId,
+    firstName,
+    lastName,
+    ssn,
+    dob,
+    address,
+    bankAcc,
+    bankRouting,
+    callback
+  ) => {
 
-        // Create user document
-        var user = {};
-        if (firstName) {
-            user.firstName = firstName;
+    // Create user document
+    let user = {};
+    firstName && (user.firstName = firstName);
+    lastName && (user.lastName = lastName);
+    address && (user.address = address);
+    bankAcc && (user.bankAcc = bankAcc);
+    bankRouting && (user.bankRouting = bankRouting);
+    ssn && (user.ssn = ssn);
+    dob && (user.dob = dob);
+    /*
+     // Fix for A7 - Sensitive Data Exposure
+     // Store encrypted ssn and DOB
+     if(ssn) {
+     user.ssn = encrypt(ssn);
+     }
+     if(dob) {
+     user.dob = encrypt(dob);
+     }
+     */
+
+    users.update(
+      {_id: parseInt(userId)},
+      {$set: user},
+      (err, result) => {
+        if (!err) {
+          console.log("Updated user profile");
+          return callback(null, user);
         }
-        if (lastName) {
-            user.lastName = lastName;
-        }
-        if (address) {
-            user.address = address;
-        }
-        if (bankAcc) {
-            user.bankAcc = bankAcc;
-        }
-        if (bankRouting) {
-            user.bankRouting = bankRouting;
-        }
-        if (ssn) {
-            user.ssn = ssn;
-        }
-        if (dob) {
-            user.dob = dob;
-        }
+
+        return callback(err, null);
+      }
+    );
+  };
+
+  this.getByUserId = (userId, callback) => {
+    users.findOne(
+      {_id: parseInt(userId)},
+      (err, user) => {
+        if (err) return callback(err, null);
         /*
-        // Fix for A7 - Sensitive Data Exposure
-        // Store encrypted ssn and DOB
-        if(ssn) {
-            user.ssn = encrypt(ssn);
-        }
-        if(dob) {
-            user.dob = encrypt(dob);
-        }
-        */
+         // Fix for A7 - Sensitive Data Exposure
+         // Decrypt ssn and DOB values to display to user
+         user.ssn = user.ssn ? decrypt(user.ssn) : "";
+         user.dob = user.dob ? decrypt(user.dob) : "";
+         */
 
-        users.update({
-                _id: parseInt(userId)
-            }, {
-                $set: user
-            },
-            function(err, result) {
-                if (!err) {
-                    console.log("Updated user profile");
-                    return callback(null, user);
-                }
-
-                return callback(err, null);
-            }
-        );
-    };
-
-    this.getByUserId = function(userId, callback) {
-        users.findOne({
-                _id: parseInt(userId)
-            },
-            function(err, user) {
-                if (err) return callback(err, null);
-                /*
-                // Fix for A7 - Sensitive Data Exposure
-                // Decrypt ssn and DOB values to display to user
-                user.ssn = user.ssn ? decrypt(user.ssn) : "";
-                user.dob = user.dob ? decrypt(user.dob) : "";
-                */
-
-                callback(null, user);
-            }
-        );
-    };
+        callback(null, user);
+      }
+    );
+  };
 }
 
 module.exports.ProfileDAO = ProfileDAO;
